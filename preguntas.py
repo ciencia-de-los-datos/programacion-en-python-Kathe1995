679d0e4512b1bd367343c13f22f06a54b2fde2b4
Kathe1995 <laacevedog@unal.edu.co>
2023-04-15 18:12:26 -0500

    entregalab1

--- preguntas.py
+++ preguntas.py
@@ -11,250 +11,133 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 """
+data=open('D:/Users/lacevedog/OneDrive - Linea Directa S.A.S/Escritorio/data.csv','r')
+data=[z.replace("\n", "") for z in data]
+data=[z.split("\t") for z in data]
 
 
 def pregunta_01():
-    """
-    Retorne la suma de la segunda columna.
-
-    Rta/
-    214
-
-    """
-    return
+    columna2=[int(z[1])for z in data]
+    return sum(columna2)
 
 
 def pregunta_02():
-    """
-    Retorne la cantidad de registros por cada letra de la primera columna como la lista
-    de tuplas (letra, cantidad), ordendas alfabéticamente.
-
-    Rta/
-    [
-        ("A", 8),
-        ("B", 7),
-        ("C", 5),
-        ("D", 6),
-        ("E", 14),
-    ]
-
-    """
-    return
+    columna1=[k[0] for k in data]
+    lista=[]
+    for i in sorted(list(set(columna1))):
+        lista.append((i,columna1.count(i)))
+    return lista
 
 
 def pregunta_03():
-    """
-    Retorne la suma de la columna 2 por cada letra de la primera columna como una lista
-    de tuplas (letra, suma) ordendas alfabeticamente.
-
-    Rta/
-    [
-        ("A", 53),
-        ("B", 36),
-        ("C", 27),
-        ("D", 31),
-        ("E", 67),
-    ]
-
-    """
-    return
+    columna1=[k[0] for k in data]
+    lista1=[]
+    for i in sorted(list(set(columna1))):
+        filtrado=[z for z in data if z[0]==i]
+        columna2f=[int(w[1]) for w in filtrado]
+        lista1.append((i, sum(columna2f)))
+    return lista1
 
 
+data = [z + [z[2].split("-")[1]] for z in data]
 def pregunta_04():
-    """
-    La columna 3 contiene una fecha en formato `YYYY-MM-DD`. Retorne la cantidad de
-    registros por cada mes, tal como se muestra a continuación.
-
-    Rta/
-    [
-        ("01", 3),
-        ("02", 4),
-        ("03", 2),
-        ("04", 4),
-        ("05", 3),
-        ("06", 3),
-        ("07", 5),
-        ("08", 6),
-        ("09", 3),
-        ("10", 2),
-        ("11", 2),
-        ("12", 3),
-    ]
-
-    """
-    return
-
+    columnam=[k[5] for k in data]
+    lista2=[]
+    for i in sorted(list(set(columnam))):
+        lista2.append((i,columnam.count(i)))
+    return lista2
 
 def pregunta_05():
-    """
-    Retorne una lista de tuplas con el valor maximo y minimo de la columna 2 por cada
-    letra de la columa 1.
-
-    Rta/
-    [
-        ("A", 9, 2),
-        ("B", 9, 1),
-        ("C", 9, 0),
-        ("D", 8, 3),
-        ("E", 9, 1),
-    ]
-
-    """
-    return
+    columna1=[k[0] for k in data]
+    lista3=[]
+    for i in sorted(list(set(columna1))):
+        filtrado=[z for z in data if z[0]==i]
+        columna2f=[int(w[1]) for w in filtrado]
+        lista3.append((i, max(columna2f),min(columna2f)))  
+    return lista3
 
 
 def pregunta_06():
-    """
-    La columna 5 codifica un diccionario donde cada cadena de tres letras corresponde a
-    una clave y el valor despues del caracter `:` corresponde al valor asociado a la
-    clave. Por cada clave, obtenga el valor asociado mas pequeño y el valor asociado mas
-    grande computados sobre todo el archivo.
+    lista4=[]
+    columna5sep=[z[4].split(",") for z in data]
+    columna5uni=columna5sep[0]
+    for i in range(len(columna5sep)-1):
+          columna5uni+=columna5sep[i+1]
+    set_keys=sorted(list(set([j[:3] for j in columna5uni])))
+    for k in set_keys: 
+        listavalues=[int(w[4:]) for w in columna5uni if w[:3]==k]
+        lista4.append((k,min(listavalues),max(listavalues)))
 
-    Rta/
-    [
-        ("aaa", 1, 9),
-        ("bbb", 1, 9),
-        ("ccc", 1, 10),
-        ("ddd", 0, 9),
-        ("eee", 1, 7),
-        ("fff", 0, 9),
-        ("ggg", 3, 10),
-        ("hhh", 0, 9),
-        ("iii", 0, 9),
-        ("jjj", 5, 17),
-    ]
-
-    """
-    return
+    return lista4
 
 
 def pregunta_07():
-    """
-    Retorne una lista de tuplas que asocien las columnas 0 y 1. Cada tupla contiene un
-    valor posible de la columna 2 y una lista con todas las letras asociadas (columna 1)
-    a dicho valor de la columna 2.
-
-    Rta/
-    [
-        (0, ["C"]),
-        (1, ["E", "B", "E"]),
-        (2, ["A", "E"]),
-        (3, ["A", "B", "D", "E", "E", "D"]),
-        (4, ["E", "B"]),
-        (5, ["B", "C", "D", "D", "E", "E", "E"]),
-        (6, ["C", "E", "A", "B"]),
-        (7, ["A", "C", "E", "D"]),
-        (8, ["E", "D", "E", "A", "B"]),
-        (9, ["A", "B", "E", "A", "A", "C"]),
-    ]
-
-    """
-    return
+    lista5=[]
+    columna2=[int(z[1])for z in data]
+    for i in sorted(list(set(columna2))):
+        filtradocol=[z for z in data if z[1]==str(i)]
+        columna1f=[w[0] for w in filtradocol]
+        lista5.append((i,columna1f))
+    return lista5
 
 
 def pregunta_08():
-    """
-    Genere una lista de tuplas, donde el primer elemento de cada tupla contiene  el valor
-    de la segunda columna; la segunda parte de la tupla es una lista con las letras
-    (ordenadas y sin repetir letra) de la primera  columna que aparecen asociadas a dicho
-    valor de la segunda columna.
-
-    Rta/
-    [
-        (0, ["C"]),
-        (1, ["B", "E"]),
-        (2, ["A", "E"]),
-        (3, ["A", "B", "D", "E"]),
-        (4, ["B", "E"]),
-        (5, ["B", "C", "D", "E"]),
-        (6, ["A", "B", "C", "E"]),
-        (7, ["A", "C", "D", "E"]),
-        (8, ["A", "B", "D", "E"]),
-        (9, ["A", "B", "C", "E"]),
-    ]
-
-    """
-    return
-
+    lista6=[]
+    columna2=[int(z[1])for z in data]
+    for i in sorted(list(set(columna2))):
+        filtradocol=[z for z in data if z[1]==str(i)]
+        columna1f=[w[0] for w in filtradocol]
+        lista6.append((i,sorted(list(set(columna1f)))))
+    return lista6
 
 def pregunta_09():
-    """
-    Retorne un diccionario que contenga la cantidad de registros en que aparece cada
-    clave de la columna 5.
+    dictionary={}
+    columna5sep=[z[4].split(",") for z in data]
+    columna5uni=columna5sep[0]
+    for i in range(len(columna5sep)-1):
+          columna5uni+=columna5sep[i+1]
+    set_keys=sorted(list(set([j[:3] for j in columna5uni])))
+    for k in set_keys: 
+        listavalues=[int(w[4:]) for w in columna5uni if w[:3]==k]
+        dictionary[k]=len(listavalues)
 
-    Rta/
-    {
-        "aaa": 13,
-        "bbb": 16,
-        "ccc": 23,
-        "ddd": 23,
-        "eee": 15,
-        "fff": 20,
-        "ggg": 13,
-        "hhh": 16,
-        "iii": 18,
-        "jjj": 18,
-    }
-
-    """
-    return
-
+    return dictionary
 
 def pregunta_10():
-    """
-    Retorne una lista de tuplas contengan por cada tupla, la letra de la columna 1 y la
-    cantidad de elementos de las columnas 4 y 5.
-
-    Rta/
-    [
-        ("E", 3, 5),
-        ("A", 3, 4),
-        ("B", 4, 4),
-        ...
-        ("C", 4, 3),
-        ("E", 2, 3),
-        ("E", 3, 3),
-    ]
-
-
-    """
-    return
+    lista7=[]
+    columna1=[z[0] for z in data]
+    columna4=[z[3].split(",") for z in data]
+    columna5=[z[4].split(",") for z in data]
+    for i in range(len(columna1)):
+        lista7.append((columna1[i],len(columna4[i]),len(columna5[i])))
+    return lista7
 
 
 def pregunta_11():
-    """
-    Retorne un diccionario que contengan la suma de la columna 2 para cada letra de la
-    columna 4, ordenadas alfabeticamente.
+    dictionary1={}
+    columna4=[z[3].split(",") for z in data]
+    columna2=[int(z[1]) for z in data]
+    columna4uni=columna4[0]
+    for i in range(len(columna2)-1):
+          columna4uni+=columna4[i+1]
+    for j in sorted(list(set(columna4uni))):
+        filtradocol4=[z for z in data if j in z[3]]
+        columna2f=[int(z[1]) for z in filtradocol4]
+        dictionary1[j]=sum(columna2f)
 
-    Rta/
-    {
-        "a": 122,
-        "b": 49,
-        "c": 91,
-        "d": 73,
-        "e": 86,
-        "f": 134,
-        "g": 35,
-    }
-
-
-    """
-    return
+    return dictionary1
 
 
 def pregunta_12():
-    """
-    Genere un diccionario que contengan como clave la columna 1 y como valor la suma de
-    los valores de la columna 5 sobre todo el archivo.
+    dictionary2={}
+    columna1= [k[0] for k in data]
+    for j in sorted(list(set(columna1))):
+        filtradocol1=[z for z in data if z[0]==j]
+        columna5sep=[z[4].split(",") for z in filtradocol1]
+        columna5uni=columna5sep[0]
+        for i in range(len(columna5sep)-1):
+           columna5uni+=columna5sep[i+1]
+        listavalues=[int(w[4:]) for w in columna5uni]
+        dictionary2[j]=sum(listavalues)
 
-    Rta/
-    {
-        'A': 177,
-        'B': 187,
-        'C': 114,
-        'D': 136,
-        'E': 324
-    }
-
-    """
-    return
+    return dictionary2
